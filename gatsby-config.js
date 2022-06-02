0e9e1cfb9df22574c029155f4cd9d67b90ca2fdf
Ben Terry <benterry.dev@gmail.com>
2022-06-02 15:57:04 -0400

    Init setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,28 @@
 module.exports = {
-  siteMetadata: {
-    title: `Ben Terry Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Ben Terry Portfolio`,
+        siteUrl: `https://www.yourdomain.tld`
     },
-    __key: "images"
-  }]
+    plugins: [
+        "gatsby-plugin-image", 
+        "gatsby-plugin-react-helmet", 
+        "gatsby-plugin-sharp", 
+        "gatsby-transformer-sharp", 
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        },
+        {
+            resolve: 'gatsby-plugin-web-font-loader',
+            options: {
+                google: {
+                    families: ['Lato', 'Roboto']
+                }
+            }
+        }
+    ]
 };
