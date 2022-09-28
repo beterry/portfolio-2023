6c46a2ef176365fcf075c2eeaa15280c0e3a7c76
Ben Terry <benterry.dev@gmail.com>
2022-09-28 16:30:44 -0400

    tm-helper images

--- src/components/ProjectImages.js
+++ src/components/ProjectImages.js
@@ -2,12 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
 
-const ProjectImages = ({images}) => {
+// style
+import colors from '../styles/colors'
+
+const ProjectImages = ({images, mobile}) => {
     return (
-        <ImageGrid className='project-images'>
+        <ImageGrid className={`project-images`}>
             {images.map(image => 
                 <Figure key={image.caption.slice(0,10)}>
-                    <Img fluid={image.fluid} />
+                    <ImageContainer className={mobile && 'mobile'}>
+                        <Screen className={mobile && 'mobile'}>
+                            <Img fluid={image.fluid} />
+                        </Screen>
+                    </ImageContainer>
                     <Caption>{image.caption}</Caption>
                 </Figure>
             )}
@@ -15,14 +22,40 @@ const ProjectImages = ({images}) => {
     )
 }
 
-const ImageGrid = styled.div``
+const ImageGrid = styled.div`
+    
+`
+
+const ImageContainer = styled.div`
+    display: grid;
+    place-items: center;
+    background-color: ${colors.gray[10]};
+    border-radius: 8px;
+    border: 1px solid lightgray;
+    margin-bottom: .5rem;
+    overflow: hidden;
+
+    &.mobile {
+        padding: 16px;
+    }
+`
+
+const Screen = styled.div`
+    width: 100%;
+
+    
+    &.mobile {
+        max-width: 200px;
+
+        .gatsby-image-wrapper {
+            border-radius: 8px;
+            border: 1px solid lightgray;
+        }
+    }
+`
 
 const Figure = styled.figure`
-    .gatsby-image-wrapper {
-        margin-bottom: .5em;
-        border-radius: 8px;
-        border: 1px solid lightgray;
-    }
+    
 `
 
 const Caption = styled.figcaption`
